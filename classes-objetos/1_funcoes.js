01524687a50c8a82990dee13897e52fbd6338348
Angelo de Barros RIbeiro <angelo.b.ribeiro@aluno.senai.br>
2026-03-06 08:25:19 -0300

    add: criei as os arquivos e fiz uns troço ai

---
+++ classes-objetos/1_funcoes.js
@@ -0,0 +1,32 @@
+
+//função diletiva
+function porcentagem(v1,v2){
+    const v3 = v1 * (v2/100)
+   return v3
+   }
+
+function juros(v1,v2){ 
+    const v3 = v1 + (v2)
+
+}
+
+function desconto(){
+
+}
+
+
+
+
+
+
+
+
+
+
+//função arrow
+let calcPorcentagem = (v1,v2) =>{ // se tem dois itens para retornar, NECESSITA dos parenteses, senão, é opcional
+    return  v1 * (v2/100)
+}
+
+
+
---
+++ classes-objetos/2_arrow-function.js
@@ -0,0 +1,4 @@
+const exibirDataHora = () =>{
+    const data = new Date();
+    console.log(data.toLocaleString());
+};//                 transforma a data em string e no GMT do computador
