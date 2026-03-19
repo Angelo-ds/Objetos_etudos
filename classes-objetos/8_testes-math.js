87bca10159a8943f2d861accc6ea69952ee7577a
Angelo <angelo.b.ribeiro@aluno.senai.br>
2026-03-19 08:18:42 -0300

    add: adicionei um novo arquivo para testes matemáticos

---
+++ classes-objetos/8_testes-math.js
@@ -0,0 +1,41 @@
+//Começo de do metodo de matemática
+console.log(Math.PI);
+confirm.log(Math.SQRT2);
+
+
+
+console.log(Math.round(4.4));
+console.log(Meth.floor(4.6));
+console.log(Meth.ceil(4.1));
+
+
+const arrendondarTroco = valor => Math.round(valor*20 )/ 20;
+console.log(arrendondarTroco(2.17));
+
+console.log(Math.random());
+
+const numeroSorteado = () => Math.floor(Math.random() * 10)+1;
+
+const rolarDado = () => Math.floor(Math.random() * 6) + 1
+console.log(rolarDado());
+
+console.log(Math.pow(2,3));
+console.log(2**3);
+console.log(Math.sqrt(25));
+console.log(Math.abs(-50));
+
+
+
+const calcularMontante = (capital,taxa,meses) => capital * meses * taxa
+
+
+
+
+
+
+
+
+
+
+
+const numeroAleatorioParOuImpar = ()
